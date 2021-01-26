d41eda174b29ab72c3cfba5ef4fd4e0b498d6a5e
ItalianCucumber <cetriolo.it@protonmail.ch>
2021-01-26 06:29:15 -0600

    Add comments

--- commands/dice.js
+++ commands/dice.js
@@ -2,15 +2,15 @@ function dice(msg) {
 	let num = 0;
 	let messageArray = msg.content.split(' ')
 
-	if ((messageArray.length == 2 && messageArray[1] == 0) || messageArray[2] == 0) {
+	if ((messageArray.length == 2 && messageArray[1] == 0) || messageArray[2] == 0) { // if number is 0
 		num = "Bruh, you think I am stupid or something?";
-	} else if (messageArray.length == 2) {
+	} else if (messageArray.length == 2) { // if only one number
 		while (num == 0) {
-			num = Math.floor(Math.random() * messageArray[1]);
+			num = Math.floor(Math.random() * messageArray[1]); // roll new number that isnt 0
 		}
-	} else if (messageArray.length >= 3) {
+	} else if (messageArray.length >= 3) { // if two numbers
 		while (num == 0) {
-			num = Math.floor(Math.random() * (messageArray[2] - messageArray[1])) + Number(messageArray[1]);
+			num = Math.floor(Math.random() * (messageArray[2] - messageArray[1])) + Number(messageArray[1]); // roll number between two inputted that is not 0
 		}
 	} else {
 		num = "You need to input a number to roll the die"
